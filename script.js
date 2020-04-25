25c53a4116cc8851d601ea4c9c08e176545cf5aa
DanilaUshackov <63172108+DanilaUshackov@users.noreply.github.com>
2020-04-25 20:13:57 +0300

    Hi

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	$('.slider').slick({
+		arrows:false, /* отображение стрелок */
+		dots: false, /* отображение точек (булитов) */
+		infinite:false,
+		centerMode:false,
+	});
+});
